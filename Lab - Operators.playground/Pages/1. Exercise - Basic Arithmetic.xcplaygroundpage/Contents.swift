84a54f43e89b0b163bcae72f2849d1cf82447013
Shanae Ullman <shanae.ullman@gmail.com>
2018-09-13 12:43:32 -0500

    SU edits

--- Lab - Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
+++ Lab - Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
@@ -3,29 +3,37 @@
  
  You decide to build a shed and want to know beforehand the area of your yard that it will take up. Create two constants, `width` and `height`, with values of 10 and 20, respectively. Create an `area` constant that is the result of multiplying the two previous constants together, and print out the result.
  */
- let width=10
+//SU edits
+let width=10
 let height=20
 let area=width*height
+// print(area)
+/*:
+ You decide that you'll divide your shed into two rooms. You want to know if dividing it equally will leave enough room for some of your larger storage items. Create a `roomArea` constant that is the result of dividing `area` in half. Print out the result.
+ */
+
 /*:
  You decide that you'll divide your shed into two rooms. You want to know if dividing it equally will leave enough room for some of your larger storage items. Create a `roomArea` constant that is the result of dividing `area` in half. Print out the result.
  */
 let roomArea=area/2
-
+// print(roomArea)
 /*:
  Create a `perimeter` constant whose value equals `width` plus `width` plus `height` plus `height`, then print out the result.
  */
 let perimeter=width+width+height+height
-print(perimeter)
+print(perimeter) // good
 /*:
  Print what you would expect the result of integer division of 10 divided by 3 to be. Create a constant, `integerDivisionResult` that is the result of 10 divided by 3, and print the value.
  */
+//print(3)
 let intergerDivisionResult=10/3
 print(intergerDivisionResult)
 /*:
  Now create two constants, `double10` and `double3`, set to 10 and 3, and declare their types as `Double` values. Declare a final constant `divisionResult` equal to the result of `double10` divided by `double3`. Print the value of `divisionResult`. How does this differ from the value when using integer division?
  */
-let double10=Double(10)
-let double3=Double(3)
+
+let double10=Double(10) // let double10: Double = 10
+let double3=Double(3) //let double3: Double = 3
 let divisionResult=double10/double3
 print(divisionResult) // Differs because double allows us to use decimal point accuracy where integer does not
 /*:
